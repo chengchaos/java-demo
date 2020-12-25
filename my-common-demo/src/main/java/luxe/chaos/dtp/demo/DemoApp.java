139d9c4af177846510ef48688f1c1cbf033d3aa4
程超 <chengchaos@outlook.com>
2020-12-26 00:16:07 +0800

    for logback

--- my-common-demo/src/main/java/luxe/chaos/dtp/demo/DemoApp.java
+++ my-common-demo/src/main/java/luxe/chaos/dtp/demo/DemoApp.java
@@ -1,10 +1,16 @@
 package luxe.chaos.dtp.demo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DemoApp {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(DemoApp.class);
 
     public static void main(String[] args) {
 
 
-
+        LOGGER.info("it works.");
     }
 }
